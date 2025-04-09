3c407e7ac418d5020bd83317f0d4949a917c9ead
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-04-09 16:14:57 -0300

    Atualização do database

--- app/database.py
+++ app/database.py
@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from dotenv import load_dotenv
+import os
 # Configuração do MYSQL
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "Mx01z++-_"
-MYSQL_HOST = "localhost"
-MYSQL_PORT = "3306"
-MYSQL_DATABASE = "feed_db"
+load_dotenv()
+# Carrega as variáveis de ambiente do arquivo .env
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
